5a5cfcc5cefcfd440ddeff51b9bc8038b1fc338f
suyueche <suyue.chen@intel.com>
2020-06-19 01:15:25 +0800

    test log collect 5

--- run_iLit.groovy
+++ run_iLit.groovy
@@ -110,13 +110,15 @@ node( sub_node_label ) {
             sh '''#!/bin/bash -x
                 echo "Running ---- ${framework}, ${model} ----"
                 # copy examples
+                log=${WORKSPACE}/${framework}/${model}
+                mkdir -p ${log}
                 rm -rf ${WORKSPACE}/ilit-models/examples
                 cp -r ${WORKSPACE}/ilit-validation/examples ${WORKSPACE}/ilit-models/
                 #timeout 1800 bash ${WORKSPACE}/ilit-validation/scripts/run_${framework}.sh \
                 #    --model=${model} \
                 #    --conda_env_name=${framework}-${framework_version} \
                 #    > ${WORKSPACE}/${framework}-${model}.log 2>&1 
-                echo "test" | tee ${WORKSPACE}/${framework}/${model}/${framework}-${model}.log 2>&1
+                echo "test" > ${log}/${framework}-${model}.log 2>&1
             '''
         }
         
