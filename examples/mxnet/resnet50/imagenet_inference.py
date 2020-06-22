559319d0916813aabd27345dbaabcfa4eed2ea5c
suyueche <suyue.chen@intel.com>
2020-06-23 00:03:18 +0800

    debug mxnet run model scripts 4

--- examples/mxnet/resnet50/imagenet_inference.py
+++ examples/mxnet/resnet50/imagenet_inference.py
@@ -92,9 +92,9 @@ def score(sym, arg_params, aux_params, data, devs, label_name, max_num_examples,
             break
 
     speed = num / (time.time() - tic)
-    top1 = eval(m.get()[0][1])
+    #top1 = eval(m.get()[0][1])
 
-    return top1, speed
+    return m.get()[0][1], speed
 
 
 
@@ -357,7 +357,7 @@ if __name__ == '__main__':
             top1, speed = score(sym, arg_params, aux_params, data, [ctx], label_name,
                   max_num_examples=num_inference_images, logger=logger)
             print("accuracy batch_size: %d" % batch_size)
-            print("q_model accuracy: %.3f " % top1)
+            print("q_model accuracy: %s " % top1)
             print("throughput batch_size: %d" % batch_size)
             print("q_model throughput: %.3f images/sec" % speed)
 
@@ -370,7 +370,7 @@ if __name__ == '__main__':
             top1, speed = score(sym, arg_params, aux_params, data, [ctx], label_name,
                                 max_num_examples=num_inference_images, logger=logger)
             print("accuracy batch_size: %d" % batch_size)
-            print("input_model accuracy: %.3f " % top1)
+            print("input_model accuracy: %s " % top1)
             print("throughput batch_size: %d" % batch_size)
             print("input_model throughput: %.3f images/sec" % speed)
 
