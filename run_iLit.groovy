e66dc754d2d8ebff5e5d29089a48b41187f13785
suyueche <suyue.chen@intel.com>
2020-07-06 10:25:56 +0800

    add cache clean

--- run_iLit.groovy
+++ run_iLit.groovy
@@ -140,6 +140,16 @@ node( sub_node_label ) {
                 echo "-------w-------"
                 w
                 echo "-------w-------"
+                echo "=======cache clean======="
+                
+                echo never  > /sys/kernel/mm/transparent_hugepage/enabled; sleep 1
+                echo never  > /sys/kernel/mm/transparent_hugepage/defrag; sleep 1
+                echo always > /sys/kernel/mm/transparent_hugepage/enabled; sleep 1
+                echo always > /sys/kernel/mm/transparent_hugepage/defrag; sleep 1
+                echo 1 > /proc/sys/vm/compact_memory; sleep 1
+                echo 3 > /proc/sys/vm/drop_caches; sleep 1
+
+                echo "=======cache clean======="
                 bash ${WORKSPACE}/ilit-validation/scripts/run_${framework}.sh \
                     --model=${model} \
                     --conda_env_name=${framework}-${framework_version} \
