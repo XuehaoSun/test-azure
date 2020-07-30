def779a4ac6a021693e96a1b0b21c2a00d609325
Myrcha, Bartosz <bartosz.myrcha@intel.com>
2020-07-30 06:37:28 -0700

    Enable modifying dataset path for pytorch image recogition models' config

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -347,9 +347,18 @@ function get_strategy {
         exit 1
     fi
 
+    update_yaml_params=""
     # Replace tuning strategy in yaml file
     if [ "${tuning_strategy}" != "" ]; then
-        python ${WORKSPACE}/ilit-validation/scripts/update_yaml_config.py --yaml=${yaml_config} --strategy=${tuning_strategy}
+        update_yaml_params="${update_yaml_params} --strategy=${tuning_strategy}"
+    fi
+
+    if [ "${framework}" == "pytorch" ] && [ "${model_type}" == "cnn" ]; then
+        update_yaml_params="${update_yaml_params} --calib-data=${dataset_location}/train --eval-data=${dataset_location}/val"
+    fi
+
+    if [ "${update_yaml_params}" != "" ]; then
+        python ${WORKSPACE}/ilit-validation/scripts/update_yaml_config.py --yaml=${yaml_config} ${update_yaml_params}
     fi
 
     count=$(grep -c 'strategy: ' "${yaml_config}") || true  # Prevent from exiting when 'strategy' not found
--- scripts/update_yaml_config.py
+++ scripts/update_yaml_config.py
@@ -1,26 +1,53 @@
-import yaml
+import ruamel.yaml as yaml
 import argparse
+import re
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--yaml", type=str, required=True, help="Path to yaml config.")
-parser.add_argument("--strategy", type=str, required=True, help="Strategy to update.")
+parser.add_argument("--strategy", type=str, required=False, help="Strategy to update.")
+parser.add_argument("--calib-data", type=str, required=False, help="Path to calibration dataset.")
+parser.add_argument("--eval-data", type=str, required=False, help="Path to evaluation dataset.")
 args = parser.parse_args()
+
+if not (args.strategy or args.calib_data or args.eval_data):
+    raise Exception("No changes. Please specify strategy, calib_data or eval_data parameter to make changes in yaml file.")
 
 tuning_config = {}
 with open(args.yaml) as yaml_file:
-    yaml_config = yaml.load(yaml_file)
+    yaml_config = yaml.round_trip_load(yaml_file, preserve_quotes=True)
 
-tuning_config = yaml_config.get("tuning", {})
-if isinstance(tuning_config, list):
-    for config in tuning_config:
-        strategy = config.get("strategy", None)
-        config.update({"strategy": args.strategy})
+if args.strategy:
+    tuning_config = yaml_config.get("tuning", {})
+    if isinstance(tuning_config, list):
+        for config in tuning_config:
+            strategy = config.get("strategy", None)
+            config.update({"strategy": args.strategy})
+            print(f"Changed {strategy} to {args.strategy}")
+    else:
+        strategy = tuning_config.get("strategy", None)
+        tuning_config.update({"strategy": args.strategy})
         print(f"Changed {strategy} to {args.strategy}")
-else:
-    strategy = tuning_config.get("strategy", None)
-    tuning_config.update({"strategy": args.strategy})
-    print(f"Changed {strategy} to {args.strategy}")
+
+if args.calib_data:
+    dataset = yaml_config.get("calibration", {}).get("dataloader", {}).get("dataset", {})
+    for item in dataset:
+        if "root" in item.keys():
+            calib_data = item.get("root")
+            item.update({"root": args.calib_data})
+            print(f"Replaced calibration dataset path from {calib_data} to {args.calib_data}.")
+
+if args.eval_data:
+    dataset = yaml_config.get("evaluation", {}).get("dataloader", {}).get("dataset", {})
+    for item in dataset:
+        if "root" in item.keys():
+            eval_data = item.get("root")
+            item.update({"root": args.eval_data})
+            print(f"Replaced evaluation dataset path from {eval_data} to {args.eval_data}.")
 
 print(f"Saving yaml config back to {args.yaml}")
+
+yaml_content = yaml.round_trip_dump(yaml_config)
+
 with open(args.yaml, 'w') as output_file:
-    yaml.dump(yaml_config, output_file, default_flow_style=False)
+    output_file.write(yaml_content)
