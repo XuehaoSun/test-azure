ce4987f35ec116ef7f20f9ca28ca0af3f97b8a2f
suyueche <suyue.chen@intel.com>
2020-08-22 09:27:50 +0800

    add binary build scripts

---
+++ iLiT_binary_build.groovy
@@ -0,0 +1,152 @@
+credential = "lab_tfbot"
+
+node_label = "clx8280"
+if ('node_label' in params && params.node_label != '') {
+    node_label = params.node_label
+}
+echo "Running on node ${node_label}"
+
+conda_env = "HOSTNAME"
+if ('conda_env' in params && params.conda_env != '') {
+    conda_env = params.conda_env
+}
+echo "Running ut on ${conda_env}"
+
+ilit_url="https://gitlab.devtools.intel.com/chuanqiw/auto-tuning.git"
+if ('ilit_url' in params && params.ilit_url != ''){
+    ilit_url = params.ilit_url
+}
+echo "ilit_url is ${ilit_url}"
+
+nigthly_test_branch = ''
+MR_source_branch = ''
+MR_target_branch = ''
+if ('nigthly_test_branch' in params && params.nigthly_test_branch != '') {
+    nigthly_test_branch = params.nigthly_test_branch
+
+}else{
+    MR_source_branch = params.MR_source_branch
+    MR_target_branch = params.MR_target_branch
+}
+echo "nigthly_test_branch: $nigthly_test_branch"
+echo "MR_source_branch: $MR_source_branch"
+echo "MR_target_branch: $MR_target_branch"
+
+binary_class = "wheel"
+if ('binary_class' in params && params.binary_class != ''){
+    binary_class = params.binary_class
+}
+echo "binary_class is ${binary_class}"
+
+def cleanup() {
+
+    try {
+        sh '''#!/bin/bash -x
+        cd $WORKSPACE
+        sudo rm -rf *           
+        '''
+    } catch(e) {
+        echo "==============================================="
+        echo "ERROR: Exception caught in cleanup()           "
+        echo "ERROR: ${e}"
+        echo "==============================================="
+
+        echo ' '
+        echo "Error while doing cleanup"
+    }
+
+}
+
+def download() {
+    dir(WORKSPACE) {
+
+        checkout scm
+
+        if(MR_source_branch != ''){
+            checkout changelog: true, poll: true, scm: [
+                    $class                           : 'GitSCM',
+                    branches                         : [[name: "${MR_source_branch}"]],
+                    browser                          : [$class: 'AssemblaWeb', repoUrl: ''],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions                       : [
+                            [$class: 'RelativeTargetDirectory', relativeTargetDir: "ilit-models"],
+                            [$class: 'CloneOption', timeout: 60],
+                            [$class: 'PreBuildMerge', options: [fastForwardMode: 'FF', mergeRemote: 'origin', mergeStrategy: 'DEFAULT', mergeTarget: "${MR_target_branch}"]]
+                    ],
+                    submoduleCfg                     : [],
+                    userRemoteConfigs                : [
+                            [credentialsId: "${credential}",
+                             url          : "${ilit_url}"]
+                    ]
+            ]
+        }
+        else {
+            checkout changelog: true, poll: true, scm: [
+                    $class                           : 'GitSCM',
+                    branches                         : [[name: "${nigthly_test_branch}"]],
+                    browser                          : [$class: 'AssemblaWeb', repoUrl: ''],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions                       : [
+                            [$class: 'RelativeTargetDirectory', relativeTargetDir: "ilit-models"],
+                            [$class: 'CloneOption', timeout: 60]
+                    ],
+                    submoduleCfg                     : [],
+                    userRemoteConfigs                : [
+                            [credentialsId: "${credential}",
+                             url          : "${ilit_url}"]
+                    ]
+            ]
+        }
+    }
+}
+
+def do_binary_build() {
+    if (binary_class == 'wheel') {
+        sh """#!/bin/bash
+            echo "Create conda env..."
+            export PATH=${HOME}/miniconda3/bin/:$PATH
+            if [ \$(conda info -e | grep ${conda_env} | wc -l) == 0 ]; then
+                conda create python=3.6.9 -y -n ${conda_env}
+            else    
+                echo "${conda_env} exist!"
+            fi
+            source activate ${conda_env}
+            
+            echo "Build binary..."
+            cd ilit-models
+            python3 setup.py sdist bdist_wheel
+        """
+    } else if (binary_class == 'conda') {
+        sh """#!/bin/bash
+            echo "TODO"
+        """
+
+    } else {
+        echo "DO NOT support ${binary_class} build!!!"
+    }
+}
+
+node(node_label){
+    try{
+        cleanup()
+        stage("download"){
+            download()
+        }
+        stage("binary build") {
+            echo "+---------------- ${binary_class} build ----------------+"
+            retry(3){
+                do_binary_build()
+            }
+        }
+
+    }catch(e){
+        currentBuild.result = "FAILURE"
+        throw e
+    }finally {
+        // archive artifacts
+        stage("Artifacts") {
+            archiveArtifacts artifacts: 'ilit*.whl', excludes: null
+            fingerprint: true
+        }
+    }
+}
