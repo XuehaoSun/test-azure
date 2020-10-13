70a93ca0b0520e74f63b11c29e4611e364123ea4
suyueche <suyue.chen@intel.com>
2020-10-13 18:48:20 +0800

    bug fix for mr report

--- scripts/generate_ilit_report_mr.sh
+++ scripts/generate_ilit_report_mr.sh
@@ -268,7 +268,7 @@ function generate_html_core {
                 }
                 printf("<td style=\"%s\" colspan=2>%.4f</td>", status_png, target);
             }else {
-              if(a == "nan" || b == "nan") {
+              if(a == nan || b == nan) {
                 printf("<td class=\"col-cell col-cell3\" colspan=2></td>");
               }else {
                 status_png = "background-color:#FFD2D2";
