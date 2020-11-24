9d33c32d5f44710ca4f7734baf50fce9aca1e90a
bartosz.myrcha@intel.com <bartosz.myrcha@intel.com>
2020-11-24 15:10:30 +0100

    Fix reading framework versions from top job

--- iLiT_generate_report.groovy
+++ iLiT_generate_report.groovy
@@ -5,7 +5,10 @@ try{ echo "tensorflow_version=${tensorflow_version}"; } catch (Exception e) { te
 try{ echo "mxnet_version=${mxnet_version}"; } catch (Exception e) { mxnet_version="" ; echo "Could not found MXNet TensorFlow version." }
 try{ echo "pytorch_version=${pytorch_version}"; } catch (Exception e) { pytorch_version="" ; echo "Could not found PyTorch version." }
 
-
+@NonCPS
+def jsonParse(def json) {
+    new groovy.json.JsonSlurperClassic().parseText(json)
+}
 
 node(NODE_NAME) {
 
@@ -21,40 +24,54 @@ node(NODE_NAME) {
         copyArtifacts(
             projectName: JOB_NAME,
             selector: specific("${BUILD_NUMBER}"),
-            filter: 'summary.log, tuning_info.log',
+            filter: 'summary.log, tuning_info.log, fw_versions.json',
             fingerprintArtifacts: true,
             target: "${WORKSPACE}/logs")
+        try {
+            fw_versions = jsonParse(readFile("$WORKSPACE/logs/fw_versions.json"))
+            tensorflow_version = fw_versions."tensorflow"
+            mxnet_version = fw_versions."mxnet"
+            pytorch_version = fw_versions."pytorch"
+        } catch (Exception e) {
+            print("Could not load framework versions.")
+        }
     }
 
     stage("Generate excel report") {
-        sh '''#!/bin/bash
-            set -xe
-
-            export PATH=${HOME}/miniconda3/bin/:$PATH
-            conda_env_name="report_generator"
-            if [ $(conda info -e | grep ${conda_env_name} | wc -l) != 0 ]; then
-                conda remove --name ${conda_env_name} --all -y
-
-                conda_dir=$(dirname $(dirname $(which conda)))
-                if [ -d ${conda_dir}/envs/${conda_env_name} ]; then
-                    rm -rf ${conda_dir}/envs/${conda_env_name}
+        withEnv([
+            "tensorflow_version=${tensorflow_version}",
+            "mxnet_version=${mxnet_version}",
+            "pytorch_version=${pytorch_version}",
+        ]) {
+            sh '''#!/bin/bash
+                set -xe
+    
+                export PATH=${HOME}/miniconda3/bin/:$PATH
+                conda_env_name="report_generator"
+                if [ $(conda info -e | grep ${conda_env_name} | wc -l) != 0 ]; then
+                    conda remove --name ${conda_env_name} --all -y
+    
+                    conda_dir=$(dirname $(dirname $(which conda)))
+                    if [ -d ${conda_dir}/envs/${conda_env_name} ]; then
+                        rm -rf ${conda_dir}/envs/${conda_env_name}
+                    fi
                 fi
-            fi
-
-            conda create python=${python_version} -y -n ${conda_env_name}
-
-            source activate ${conda_env_name}
-
-            pip install -U pip
-
-            pip install -r ./ilit-validation/scripts/report_generator/requirements.txt
-            python ./ilit-validation/scripts/report_generator/generate_excel_report.py \
-                --tuning-log=${WORKSPACE}/logs/tuning_info.log \
-                --summary-log=${WORKSPACE}/logs/summary.log \
-                --tensorflow-version=${tensorflow_version} \
-                --mxnet-version=${mxnet_version} \
-                --pytorch-version=${pytorch_version}
-        '''
+    
+                conda create python=${python_version} -y -n ${conda_env_name}
+    
+                source activate ${conda_env_name}
+    
+                pip install -U pip
+    
+                pip install -r ./ilit-validation/scripts/report_generator/requirements.txt
+                python ./ilit-validation/scripts/report_generator/generate_excel_report.py \
+                    --tuning-log=${WORKSPACE}/logs/tuning_info.log \
+                    --summary-log=${WORKSPACE}/logs/summary.log \
+                    --tensorflow-version=${tensorflow_version} \
+                    --mxnet-version=${mxnet_version} \
+                    --pytorch-version=${pytorch_version}
+            '''
+        }
     }
 
     stage("Send report") {
