8b4bd40260afede1ab76197fd10dc08a8c965775
suyueche <suyue.chen@intel.com>
2021-02-05 14:59:02 +0800

    add conda clean

--- lpot_binary_build.groovy
+++ lpot_binary_build.groovy
@@ -161,6 +161,7 @@ def do_binary_build() {
             cp dist/lpot*.whl ${WORKSPACE}/
             
             echo "Build Conda binary..."
+            conda clean -a
             cd ${WORKSPACE}/lpot-validation/conda_build/${conda_version}
             export LPOT_WHL=${WORKSPACE}/lpot*.whl
             pip install pyyaml six 
