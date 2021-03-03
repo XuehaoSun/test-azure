aac69636edd60d0822f23945fff32028e9549240
Chen, Suyue <suyue.chen@intel.com>
2021-03-03 06:44:51 -0800

    Add install test, source and pip, also change conda build yaml to public lpot repo

--- run_lpot_installation_test.groovy
+++ run_lpot_installation_test.groovy
@@ -37,6 +37,18 @@ if (params.source_install != null){
 }
 echo "source_install = ${source_install}"
 
+pip_install=true
+if (params.pip_install != null){
+    pip_install=params.pip_install
+}
+echo "pip_install = ${pip_install}"
+
+binary_build_job=""
+if (params.binary_build_job != null){
+    binary_build_job=params.binary_build_job
+}
+echo "binary_build_job = ${binary_build_job}"
+
 def parseStrToList(srtingElements, delimiter=',') {
     if (srtingElements == ''){
         return []
@@ -105,8 +117,9 @@ node(node_label) {
 
                         withEnv(["python_version=${per_python_version}"]) {
                             sh '''#!/bin/bash
+                            set -x
                             export PATH=${HOME}/miniconda3/bin/:$PATH
-                            conda_env_name=lpot-install-${python_version}
+                            conda_env_name=lpot-install-${python_version}-test
                             if [ $(conda info -e | grep ${conda_env_name} | wc -l) != 0 ]; then
                                 conda remove --name ${conda_env_name} --all -y
                             fi
@@ -117,31 +130,118 @@ node(node_label) {
                             fi
                         
                             conda create python=${python_version} -y -n ${conda_env_name}
-                            source activate ${conda_env_name}
+                            if [ $(conda info -e | grep ${conda_env_name} | wc -l) != 0 ]; then
+                                source activate ${conda_env_name}
+                            else
+                                echo "test conda build failed on Python ${python_version} \n" >> ${WORKSPACE}/source_code_build_test.log
+                                exit 0
+                            fi
+                            
                             # Upgrade pip
                             # pip install -U pip
                             python -V
                             echo "-----pip list before requirements.txt install..."
                             pip list
-                            pip install -r requirements.txt
+                            pip install -r requirements.txt --no-cache-dir
                             echo "-----pip list after requirements.txt install..."
                             pip list
+                            git clean -df
                             python setup.py install 2>&1 | tee ${WORKSPACE}/source_install_${python_version}.log
                             echo "-----pip list after setup.py install..."
                             pip list 
+                            cd ${WORKSPACE}
                             lpot_path=$(python -c 'import lpot; import os; print(os.path.dirname(lpot.__file__))')
-                            detail_python=$(python -V)
-                            if [ lpot_path == '' ]; then
+                            echo "lpot_path:  ${lpot_path}"
+                            if [ ${lpot_path} == '' ]; then
                                 test_status="failed"
                             else
                                 test_status="pass" 
                             fi 
-                            echo "test ${test_status} on ${detail_python} \n" >> ${WORKSPACE}/source_code_build_test.log
+                            echo "test ${test_status} on Python ${python_version} \n" >> ${WORKSPACE}/source_code_build_test.log
                         '''
                         }
-
                     }
+                }
+            }
+        }
+        if (pip_install){
+            if ("${binary_build_job}" == "") {
+                stage('Build binary') {
+                    List binaryBuildParams = [
+                            string(name: "lpot_url", value: "${lpot_url}"),
+                            string(name: "lpot_branch", value: "${lpot_branch}"),
+                            string(name: "val_branch", value: "${val_branch}")
+                    ]
+                    downstreamJob = build job: "lpot-release-wheel-build", propagate: false, parameters: binaryBuildParams
 
+                    binary_build_job = downstreamJob.getNumber()
+                    echo "binary_build_job: ${binary_build_job}"
+                    echo "downstreamJob.getResult(): ${downstreamJob.getResult()}"
+                    if (downstreamJob.getResult() != "SUCCESS") {
+                        currentBuild.result = "FAILURE"
+                        failed_build_url = downstreamJob.absoluteUrl
+                        echo "failed_build_url: ${failed_build_url}"
+                        error("---- lpot wheel build got failed! ---- Details in ${failed_build_url}consoleText! ---- ")
+                    }
+                }
+            }
+            stage('Copy binary') {
+                catchError {
+                    copyArtifacts(
+                            projectName: 'lpot-release-wheel-build',
+                            selector: specific("${binary_build_job}"),
+                            filter: 'lpot*.whl',
+                            fingerprintArtifacts: true,
+                            target: "${WORKSPACE}")
+
+                    archiveArtifacts artifacts: "lpot*.whl"
+                }
+            }
+
+            stage("pip install") {
+                echo "---------------------------------------------------------"
+                echo "-----------------  pip install  -----------------"
+                echo "---------------------------------------------------------"
+
+                dir("$WORKSPACE") {
+                    python_version_list.each { per_python_version ->
+
+                        withEnv(["python_version=${per_python_version}"]) {
+                            sh '''#!/bin/bash
+                            set -x
+                            export PATH=${HOME}/miniconda3/bin/:$PATH
+                            conda_env_name=lpot-install-${python_version}-test
+                            if [ $(conda info -e | grep ${conda_env_name} | wc -l) != 0 ]; then
+                                conda remove --name ${conda_env_name} --all -y
+                            fi
+                        
+                            conda_dir=$(dirname $(dirname $(which conda)))
+                            if [ -d ${conda_dir}/envs/${conda_env_name} ]; then
+                                rm -rf ${conda_dir}/envs/${conda_env_name}
+                            fi
+                        
+                            conda create python=${python_version} -y -n ${conda_env_name}
+                            if [ $(conda info -e | grep ${conda_env_name} | wc -l) != 0 ]; then
+                                source activate ${conda_env_name}
+                            else
+                                echo "test conda build failed on Python ${python_version} \n" >> ${WORKSPACE}/pip_install_test.log
+                                exit 0
+                            fi
+                            
+                            pip install lpot*.whl --no-cache-dir 2>&1 | tee ${WORKSPACE}/pip_install_${python_version}.log
+                            
+                            pip list 
+                            lpot_path=$(python -c 'import lpot; import os; print(os.path.dirname(lpot.__file__))')
+                            echo "lpot_path:  ${lpot_path}"
+                            if [ ${lpot_path} == '' ]; then
+                                test_status="failed"
+                            else
+                                test_status="pass" 
+                            fi 
+                            echo "test ${test_status} on Python ${python_version} \n" >> ${WORKSPACE}/pip_install_test.log
+                        '''
+                        }
+                    }
                 }
             }
         }
