6bc68e67533ee7c52ab9707963403f8060823a3f
suyueche <suyue.chen@intel.com>
2021-03-19 09:49:21 +0800

    add annotations for coco dataset

--- scripts/run_tuning_trigger.sh
+++ scripts/run_tuning_trigger.sh
@@ -207,6 +207,7 @@ function update_yaml_config {
     if [ "${framework}" != "pytorch" ]; then
       sed -i "/\/path\/to\/calibration\/dataset/s|root:.*|root: $dataset_location|g" ${yaml}
       sed -i "/\/path\/to\/evaluation\/dataset/s|root:.*|root: $dataset_location|g" ${yaml}
+      sed -i "/\/path\/to\/annotation/s|anno_path:.*|anno_path: /tf_dataset/dataset/coco_dataset/raw-data/annotations/instances_val2017.json |g" ${yaml}
       if [ "${framework}" == "tensorflow" ] && [ "${model}" == "bert" ]; then
         sed -i "/\/path\/to\/eval.tf_record/s|root:.*|root: $dataset_location/eval.tf_record|g" ${yaml}
         sed -i "/\/path\/to\/dev-v1.1.json/s|label_file:.*|label_file: $dataset_location/dev-v1.1.json|g" ${yaml}
