ca2d157182269cd30214c813bbf6e630f02f5fe2
bartosz.myrcha@intel.com <bartosz.myrcha@intel.com>
2021-03-22 11:28:12 +0100

    Change pylint rules

--- scripts/run_format_scan.sh
+++ scripts/run_format_scan.sh
@@ -62,7 +62,7 @@ main() {
 
 run_pylint() {
     pip install pylint
-    python -m pylint -f json --disable=R,C,W,I,E0401,E0611 --enable=line-too-long --max-line-length=99 --extension-pkg-whitelist=numpy  --ignored-classes=TensorProto,NodeProto lpot > ${WORKSPACE}/lpot-pylint.json
+    python -m pylint -f json --disable=R,C,W --enable=line-too-long --max-line-length=99 --extension-pkg-whitelist=numpy  --ignored-classes=TensorProto,NodeProto --ignored-modules=tensorflow lpot > ${WORKSPACE}/lpot-pylint.json
     exit_code=$?
     if [ ${exit_code} -ne 0 ] ; then
         echo "PyLint exited with non-zero exit code."; exit 1
