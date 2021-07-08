32c0a8507279092eee90a3696a967a74eb5b0e5c
chen,suyue <suyue.chen@intel.com>
2021-07-08 16:32:30 +0800

    support torch 1.9.0 and torch_vision 0.10.0

--- run_lpot.groovy
+++ run_lpot.groovy
@@ -138,6 +138,7 @@ if (params.collect_tuned_model != null){
 echo "collect_tuned_model = ${collect_tuned_model}"
 
 torchvision_versions = [
+        "1.9.0": "0.10.0",
         "1.8.0": "0.9.0",
         "1.7.0": "0.8.0",
         "1.6.0": "0.7.0",
