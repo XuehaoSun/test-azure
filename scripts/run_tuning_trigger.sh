b6cdaf0f13c42e663c99c6a8955624f0ef395196
Bartosz Myrcha <bartosz.myrcha@intel.com>
2021-08-23 03:59:17 +0200

    ILITV-871 Enable PyTorch ssd_resnet34_qat_fx in validation (#25)
    * ILITV-871 Enable PyTorch ssd_resnet34_qat_fx in validation

--- scripts/run_tuning_trigger.sh
+++ scripts/run_tuning_trigger.sh
@@ -139,7 +139,7 @@ main() {
     fi
 
     if [[ "${framework}" == "pytorch" ]]; then
-        if [[ "${model}" == "rnnt" ]] || [[ "${model}" == "ssd_resnet34_fx" ]]; then
+        if [[ "${model}" == "rnnt" ]] || [[ "${model}" == "ssd_resnet34_fx" ]] || [[ "${model}" == "ssd_resnet34_qat_fx" ]]; then
             if [ ${model} == "rnnt" ];then
                 cd ${model_src_dir}/../../../utils/MLPerf/loadgen
             else
@@ -224,9 +224,11 @@ main() {
     echo "Checking topology..."
     echo "Framework: '${framework}'"
     echo "Model: '${model}'"
-    if [ "${framework}" == "pytorch" ] && [ "${model}" == "ssd_resnet34_fx" ]; then
-        topology="ssd-resnet34"
-        echo "Setting topology to ${topology}"
+    if [ "${framework}" == "pytorch" ]; then
+        if [ "${model}" == "ssd_resnet34_fx" ] || [ "${model}" == "ssd_resnet34_qat_fx" ]; then
+            topology="ssd-resnet34"
+            echo "Setting topology to ${topology}"
+        fi
     fi
 
     echo "Topology is '${topology}'"
@@ -237,7 +239,7 @@ main() {
     # pytorch need to use default output_model path
     if [ ${framework} != "pytorch" ]; then
       parameters="${parameters} --output_model=${q_model}"
-    elif [ "${model}" == "rnnt" ] || [ "${model}" == "ssd_resnet34_fx" ]; then
+    elif [ "${model}" == "rnnt" ] || [ "${model}" == "ssd_resnet34_fx" ] || [ "${model}" == "ssd_resnet34_qat_fx" ]; then
         parameters=" ${parameters} --output_model=${model_src_dir}/saved_results"
     fi
 
@@ -366,8 +368,10 @@ function update_yaml_config {
         fi
     fi
 
-    if [ "${framework}" == "pytorch" ] && [ "${model}" == "ssd_resnet34_fx" ]; then
-        sed -i "/convert_dataset\/annotations\/instances_val2017\.json/s|anno_dir:.*|anno_dir: ${dataset_location}/annotations/instances_val2017.json |g" ${yaml}
+    if [ "${framework}" == "pytorch" ]; then
+        if [ "${model}" == "ssd_resnet34_fx" ] || [ "${model}" == "ssd_resnet34_qat_fx" ]; then
+            sed -i "/convert_dataset\/annotations\/instances_val2017\.json/s|anno_dir:.*|anno_dir: ${dataset_location}/annotations/instances_val2017.json |g" ${yaml}
+        fi
     fi
 
     update_yaml_params=""
