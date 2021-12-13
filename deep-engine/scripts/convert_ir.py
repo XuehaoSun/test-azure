028c5b061f5aafc561b8c820ac16dd495ada8edb
chen,suyue <suyue.chen@intel.com>
2021-12-13 21:38:07 +0800

    update engine fp32 convert api

--- deep-engine/scripts/convert_ir.py
+++ deep-engine/scripts/convert_ir.py
@@ -1,9 +1,9 @@
 import argparse
-from engine.compile import prepare_ir
+from engine.compile import compile
 
 parser = argparse.ArgumentParser(allow_abbrev=False)
 parser.add_argument("--fp32_models", type=str, required=True)
 args = parser.parse_args()
 
-model = prepare_ir(args.fp32_models)
+model = compile(args.fp32_models)
 model.save()
