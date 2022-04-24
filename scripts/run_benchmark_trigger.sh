d6c64c8b8bdd98540c2820a329b76024370994dd
chen,suyue <suyue.chen@intel.com>
2022-04-24 11:00:09 +0800

    disable multi-instance for PT dlrm models

--- scripts/run_benchmark_trigger.sh
+++ scripts/run_benchmark_trigger.sh
@@ -218,7 +218,7 @@ function run_benchmark {
     ncores_per_instance=${ncores_per_socket}
     iters=100
 
-    single_instance=("3dunet" "centernet_hg104" "GPT2")
+    single_instance=("3dunet" "centernet_hg104" "GPT2" "dlrm" "dlrm_fx")
     if [[ " ${single_instance[@]} " =~ " ${model} " ]]; then
         multi_instance="false"
     fi
