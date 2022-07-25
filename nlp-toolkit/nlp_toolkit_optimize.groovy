8a3bf038ee9f6836872e05e31206d39ffef8eeb8
Wenxin Zhang <wenxin.zhang@intel.com>
2022-07-25 09:32:38 +0800

    add torch 1.12 for nlp-toolkit

--- nlp-toolkit/nlp_toolkit_optimize.groovy
+++ nlp-toolkit/nlp_toolkit_optimize.groovy
@@ -132,6 +132,7 @@ if (params.collect_tuned_model != null) {
 echo "collect_tuned_model = ${collect_tuned_model}"
 
 torchvision_versions = [
+        "1.12.0": "0.13.0",
         "1.11.0": "0.12.0",
         "1.10.1": "0.11.2",
         "1.10.0": "0.11.0",
