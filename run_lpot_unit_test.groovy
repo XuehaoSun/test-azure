846eb41d456d91cc089f65524935582ac752ace2
chensuyue <suyue.chen@intel.com>
2022-09-05 14:11:17 +0800

    change model copy process

--- run_lpot_unit_test.groovy
+++ run_lpot_unit_test.groovy
@@ -271,24 +271,6 @@ def build_conda_env(conda_env_name) {
             '''
         }
     }
-    // prepare env with local files to avoid network downloading problem
-    sh'''#!/bin/bash
-        set -xe
-        declare local_file_list=("mobilenet_v1_1.0_224.tgz" "slim/inception_v1_2016_08_28.tar.gz" "saved_model.tar.gz" "ssd_resnet50_v1.tgz" "cifar-10-batches-py.tar.gz" "resnet_v2")
-        local_path="/home/tensorflow/localfile"
-        declare target_path=("/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/home/tensorflow/.keras/datasets/" "/tmp/.neural_compressor/inc_ut/")  
-        mkdir -p /tmp/.neural_compressor/
-        rm -rf /home/tensorflow/.keras/datasets
-        mkdir -p /home/tensorflow/.keras/datasets
-        rm -rf /tmp/.neural_compressor/inc_ut/resnet_v2
-        for((i=0; i<${#local_file_list[@]}; i++))
-        do
-            filename=${local_file_list[i]}
-            [[ ! -f ${local_path}/${filename} && ! -d ${local_path}/${filename} ]] && continue
-            [[ -d ${local_path}/${filename%/*} ]] && mkdir -p ${target_path[i]}${filename%/*} && cp -r ${local_path}/${filename} ${target_path[i]} && continue
-            cp -r ${local_path}/${filename} ${target_path[i]}${filename}
-        done
-    '''
 }
 
 def run_coverage_test(is_base=false, MR_branch=""){
@@ -493,6 +475,24 @@ node(node_label){
         }
 
         stage('env_build') {
+            // prepare env with local files to avoid network downloading problem
+            sh'''#!/bin/bash
+                set -xe
+                declare local_file_list=("mobilenet_v1_1.0_224.tgz" "slim/inception_v1_2016_08_28.tar.gz" "saved_model.tar.gz" "ssd_resnet50_v1.tgz" "cifar-10-batches-py.tar.gz" "resnet_v2")
+                local_path="/home/tensorflow/localfile"
+                declare target_path=("/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/home/tensorflow/.keras/datasets/" "/tmp/.neural_compressor/inc_ut/")  
+                mkdir -p /tmp/.neural_compressor/
+                rm -rf /home/tensorflow/.keras/datasets
+                mkdir -p /home/tensorflow/.keras/datasets
+                rm -rf /tmp/.neural_compressor/inc_ut/resnet_v2
+                for((i=0; i<${#local_file_list[@]}; i++))
+                do
+                    filename=${local_file_list[i]}
+                    [[ ! -f ${local_path}/${filename} && ! -d ${local_path}/${filename} ]] && continue
+                    [[ -d ${local_path}/${filename%/*} ]] && mkdir -p ${target_path[i]}${filename%/*} && cp -r ${local_path}/${filename} ${target_path[i]} && continue
+                    cp -r ${local_path}/${filename} ${target_path[i]}${filename}
+                done
+            '''
             if (MR_source_branch != "" && run_coverage) {
                 // Pre-CI
                 parallel(
