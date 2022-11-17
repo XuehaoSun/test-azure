a8febabbe091e17e1f59057c433028e097e3c61b
VincyZhang <wenxin.zhang@intel.com>
2022-11-16 23:49:39 -0800

    Fix sparselib benchmark (#289)

--- sparse_lib/run_ci_trigger.sh
+++ sparse_lib/run_ci_trigger.sh
@@ -7,19 +7,19 @@ export PATH=${HOME}/miniconda3/bin/:$PATH
 conda activate $conda_env_name || source activate $conda_env_name
 conda install -c conda-forge gxx gcc cmake -y
 
-rm -rf ${WORKSPACE}/lpot-models/nlp_toolkit/backends/neural_engine/test/SparseLib/benchmark/build
+rm -rf ${WORKSPACE}/lpot-models/nlp_toolkit/backends/neural_engine/build
 cd ${WORKSPACE}/lpot-models
 git submodule update --init --recursive
-cd nlp_toolkit/backends/neural_engine/test/SparseLib/benchmark
+cd nlp_toolkit/backends/neural_engine
 mkdir build
 cd build
 if [[ -n $(lscpu | grep amx_tile) ]]; then cmake_amx="-DSPARSE_LIB_USE_AMX=True"; fi
-CC=gcc CXX=g++ cmake .. $cmake_amx
+CC=gcc CXX=g++ cmake .. -DNE_WITH_SPARSELIB=ON -DNE_WITH_TESTS=ON -DNE_WITH_SPARSELIB_BENCHMARK=ON -DPYTHON_EXECUTABLE=$(which python) $cmake_amx 
 make -j
-
-bash -x ../ci/run_ci.sh $output_log_dir
+cd bin
+bash -x ${WORKSPACE}/lpot-models/nlp_toolkit/backends/neural_engine/test/SparseLib/benchmark/ci/run_ci.sh $output_log_dir
 for caselog in $(find $output_log_dir/*); do
     case_name=$(echo $caselog | sed -e 's/\.log$//')
     echo "case_name=$case_name"
-    bash -x ../ci/to_summary.sh $caselog | tee "${case_name}_summary.log"
+    bash -x ${WORKSPACE}/lpot-models/nlp_toolkit/backends/neural_engine/test/SparseLib/benchmark/ci/to_summary.sh $caselog | tee "${case_name}_summary.log"
 done
