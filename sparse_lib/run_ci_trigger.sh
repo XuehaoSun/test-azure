a4e9f268b9a501e2e18deabfef73c29321380db7
Wenxin Zhang <wenxin.zhang@intel.com>
2022-11-30 10:43:48 +0800

    fix sparselib ci

--- sparse_lib/run_ci_trigger.sh
+++ sparse_lib/run_ci_trigger.sh
@@ -10,14 +10,14 @@ conda install -c conda-forge gxx gcc cmake -y
 rm -rf ${WORKSPACE}/lpot-models/intel_extension_for_transformers/backends/neural_engine/build
 cd ${WORKSPACE}/lpot-models
 git submodule update --init --recursive
-cd intel_extension_for_transformers/backends
+cd intel_extension_for_transformers/backends/neural_engine
 mkdir build
 cd build
 if [[ -n $(lscpu | grep amx_tile) ]]; then cmake_amx="-DSPARSE_LIB_USE_AMX=True"; fi
 CC=gcc CXX=g++ cmake .. -DNE_WITH_SPARSELIB=ON -DNE_WITH_TESTS=ON -DNE_WITH_SPARSELIB_BENCHMARK=ON -DPYTHON_EXECUTABLE=$(which python) $cmake_amx 
 make -j
 cd bin
-bash -x ${WORKSPACE}/lpot-models/nlp_toolkit/backends/neural_engine/test/SparseLib/benchmark/ci/run_ci.sh $output_log_dir
+bash -x ${WORKSPACE}/lpot-models/nlp_toolkit/backends/neural_engine/test/kernels/benchmark/ci/run_ci.sh $output_log_dir
 
 for caselog in $(find $output_log_dir/*); do
     case_name=$(echo $caselog | sed -e 's/\.log$//')
