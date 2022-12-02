697904f1f6f9393ece94c39e04d8aad06da5ab21
chensuyue <suyue.chen@intel.com>
2022-12-02 14:37:37 +0800

    add one pytorch single instance model
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- scripts/run_benchmark_trigger.sh
+++ scripts/run_benchmark_trigger.sh
@@ -222,7 +222,7 @@ function run_benchmark {
     ncores_per_instance=${ncores_per_socket}
     iters=100
 
-    single_instance=("3dunet" "centernet_hg104" "GPT2" "dlrm" "dlrm_fx" "dlrm_ipex")
+    single_instance=("3dunet" "centernet_hg104" "GPT2" "dlrm" "dlrm_fx" "dlrm_ipex" "gpt_j_wikitext")
     if [[ " ${single_instance[@]} " =~ " ${model} " ]]; then
         multi_instance="false"
     fi
