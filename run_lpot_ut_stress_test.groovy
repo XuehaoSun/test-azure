0aa33374fc3d3bcbd7b759d32244e9946626bbc9
chensuyue <suyue.chen@intel.com>
2022-12-22 12:59:35 +0800

    add catch Error for data copy
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- run_lpot_ut_stress_test.groovy
+++ run_lpot_ut_stress_test.groovy
@@ -213,22 +213,24 @@ def build_conda_env() {
         }
     }
     // prepare env with local files to avoid network downloading problem
-    sh'''#!/bin/bash
-        set -xe
-        declare local_file_list=("mobilenet_v1_1.0_224.tgz" "slim/inception_v1_2016_08_28.tar.gz" "saved_model.tar.gz" "ssd_resnet50_v1.tgz" "cifar-10-batches-py.tar.gz" "cifar-10-batches-py" "resnet_v2")
-        local_path="/tf_dataset2/inc-ut"
-        declare target_path=("/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/home/tensorflow/.keras/datasets/" "/home/tensorflow/.keras/datasets/" "/tmp/.neural_compressor/inc_ut/")
-        mkdir -p /tmp/.neural_compressor/
-        mkdir -p /home/tensorflow/.keras/datasets
-        rm -rf /tmp/.neural_compressor/inc_ut/resnet_v2
-        for((i=0; i<${#local_file_list[@]}; i++))
-        do
-            filename=${local_file_list[i]}
-            [[ ! -f ${local_path}/${filename} && ! -d ${local_path}/${filename} ]] && continue
-            [[ -d ${local_path}/${filename%/*} ]] && mkdir -p ${target_path[i]}${filename%/*} && cp -r ${local_path}/${filename} ${target_path[i]} && continue
-            cp -r ${local_path}/${filename} ${target_path[i]}${filename}
-        done
-    '''
+    catchError {
+        sh '''#!/bin/bash
+            set -xe
+            declare local_file_list=("mobilenet_v1_1.0_224.tgz" "slim/inception_v1_2016_08_28.tar.gz" "saved_model.tar.gz" "ssd_resnet50_v1.tgz" "cifar-10-batches-py.tar.gz" "cifar-10-batches-py" "resnet_v2")
+            local_path="/tf_dataset2/inc-ut"
+            declare target_path=("/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/tmp/.neural_compressor/" "/home/tensorflow/.keras/datasets/" "/home/tensorflow/.keras/datasets/" "/tmp/.neural_compressor/inc_ut/")
+            mkdir -p /tmp/.neural_compressor/
+            mkdir -p /home/tensorflow/.keras/datasets
+            rm -rf /tmp/.neural_compressor/inc_ut/resnet_v2
+            for((i=0; i<${#local_file_list[@]}; i++))
+            do
+                filename=${local_file_list[i]}
+                [[ ! -f ${local_path}/${filename} && ! -d ${local_path}/${filename} ]] && continue
+                [[ -d ${local_path}/${filename%/*} ]] && mkdir -p ${target_path[i]}${filename%/*} && cp -r ${local_path}/${filename} ${target_path[i]} && continue
+                cp -r ${local_path}/${filename} ${target_path[i]}${filename}
+            done
+        '''
+    }
 }
 
 def binary_install() {
