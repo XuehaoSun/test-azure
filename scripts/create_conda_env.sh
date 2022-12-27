84ded2868f4065d13a003e4d4d4ffd41322b31b9
chensuyue <suyue.chen@intel.com>
2022-12-27 14:56:27 +0800

    minor issue fix
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- scripts/create_conda_env.sh
+++ scripts/create_conda_env.sh
@@ -331,7 +331,7 @@ else
 fi
 
 if [ "${mxnet_version}" != '' ]; then
-    pip install numpy==1.23.5
+    pip install "numpy<=1.23.5"
     echo "re-install pycocotools resolve the issue with numpy..."
     pip uninstall pycocotools -y
     pip install --no-cache-dir pycocotools
