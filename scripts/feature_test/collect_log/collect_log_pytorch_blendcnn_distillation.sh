84ba3a0928502c17d0fcc515fa49100968c62e31
ronggegu <111265999+ronggegu@users.noreply.github.com>
2023-02-26 22:37:00 +0800

    fix pytorch_blendcnn_distillation collect log status (#351)

--- scripts/feature_test/collect_log/collect_log_pytorch_blendcnn_distillation.sh
+++ scripts/feature_test/collect_log/collect_log_pytorch_blendcnn_distillation.sh
@@ -10,28 +10,14 @@ CPU_NAME=$(cat "${WORKSPACE}/${feature_name}/cpu_name.log")
 blendcnn_distilling_log="${WORKSPACE}/${feature_name}/blendcnn-distilling-test.log"
 
 # score
-initial_score=$(
-    grep 'initial model score is' ${blendcnn_distilling_log} |sed 's/.*is *//;s/. *$//' |tail -1
+distillation_result=$(
+    grep -c 'Training has been done properly.' ${WORKSPACE}/${feature_name}/blendcnn-distilling-test.log
 )
 
-distilled_score=$(
-    grep 'distilled model score is' ${blendcnn_distilling_log} |sed 's/.*is *//;s/. *$//' |tail -1
-)
-
-# conclusion
-test_status=$(
-    echo |awk -v x=${initial_score} -v y=${distilled_score} 'BEGIN {
-        result = "check";
-    }{
-        if(x < y) {
-            result = "pass";
-        }else {
-            result = "fail";
-        }
-    }END {
-        printf("%s", result);
-    }'
-)
-
+if [[ "${distillation_result}" == "1" ]]; then
+    test_status="pass"
+elif [[ "${distillation_result}" == "0" ]]; then
+    test_status="fail"
+fi
 # summary
 echo "${CPU_NAME};${feature_name};${test_status};${BUILD_URL}artifact/${feature_name}" | tee -a ${summaryLog}
