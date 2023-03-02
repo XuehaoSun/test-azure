5c0d0a75b2ec100205177c8b344e91280d8533bb
Wenxin Zhang <wenxin.zhang@intel.com>
2023-03-02 13:42:24 +0800

    fix re match

--- nlp-toolkit/nlp_toolkit_deploy.groovy
+++ nlp-toolkit/nlp_toolkit_deploy.groovy
@@ -372,7 +372,7 @@ def runPerfTest(mode, precision, benchmark_cmd, output_path="${WORKSPACE}") {
                 export CXX=/opt/rh/gcc-toolset-11/root/usr/bin/g++
                 gcc -v
             fi
-            if [[ "${model}" == "vit_large*" ]] || [[ "${model}" == "vit_base*" ]]; then
+            if [[ "${model}" == "vit_large"* ]] || [[ "${model}" == "vit_base"* ]]; then
                 cp -r ${data_path} ${working_dir}/data
             fi
             echo "final benchmark cmd of precision ${precision} is ${benchmark_cmd}"
@@ -507,7 +507,7 @@ def prepare_models(local_precision, prepare_cmd) {
     if (cpu in nightly_cpu_list){
         cpu = cpu.split("-")[0]
     }
-    if (model == "vit_large*" || model == "vit_base*") {
+    if (model == "vit_large"* || model == "vit_base"*) {
         withEnv(["data_dir=${data_dir}"]) {
             sh '''#!/bin/bash -x
                 cp -r ${data_dir} /home/tensorflow/.cache/nlp_toolkit/vit
