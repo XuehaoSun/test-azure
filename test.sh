5bcce9bfef915746f75a1f16d28dbc88ad0317fd
XuehaoSun <xuehao.sun@intel.com>
2023-07-03 13:53:53 +0800

    test
    Signed-off-by: XuehaoSun <xuehao.sun@intel.com>

--- test.sh
+++ test.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 echo "-------1----------"
-echo ${name}
-echo "-------2----------"
-echo ${env}
-echo "-------3----------"
-echo ${env.name}
+echo "is_perf_reg=true" >> "$GITHUB_ENV"
