d1705b2ba0cdb8198b073f4d6b89561031bed3ab
Sun, Xuehao <xuehao.sun@intel.com>
2023-08-09 10:46:29 +0800

    add pre commit
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello!")
+import os
+
+str1 = "hello"
+str2 = "world"
+str3 = '!'
+assert len(str1) == len(str2) == len(str3), \
+    'Wrong length for str1:{}, str2:{} or str3:{}, \
+     all should be the same.'.format(
+        len(str1), len(str2), len(str3)
+    )
