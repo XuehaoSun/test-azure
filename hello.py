e7922a0cc8d8964aadf4d38db23a3218dc0a4c64
Sun, Xuehao <xuehao.sun@intel.com>
2023-08-16 15:43:04 +0800

    test
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- hello.py
+++ hello.py
@@ -8,7 +8,10 @@ assert len(str1) == len(str2) == len(str3), \
      all should be the same.'.format(
         len(str1), len(str2), len(str3)
     )
-
+def get_onnx_model():
+    model = torchvision.models.resnet18()
+    x = Variable(torch.randn(1, 3, 224, 224))
+    torch_out = torch.onnx.export(model, x, "resnet18.onnx", export_params=True, verbose=True)
 print("hello!!!")
 print("hello!p!!")
 print("test")
