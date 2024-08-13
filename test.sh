0c995651ea47ad563ef8e488561e3ef814512323
Sun, Xuehao <xuehao.sun@intel.com>
2024-08-13 14:45:13 +0800

    test
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- test.sh
+++ test.sh
@@ -1,4 +1,27 @@
-#!/usr/bin/env bash
-echo "-------1----------"
-echo "is_perf_reg=kkkll" >> "$GITHUB_ENV"
-echo "is_perf_reg=true" >> "$GITHUB_ENV"
+#!/bin/bash
+
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+function freeze() {
+    local file=$1
+    local folder=$(dirname "$file")
+    pip-compile --no-upgrade --output-file "$folder/freeze.txt" "$file"
+    if [[ -e "$folder/freeze.txt" ]]; then
+        mv "$folder/freeze.txt" "$file"
+    fi
+}
+
+function check_branch_name() {
+    branch_name=$(git branch --show-current)
+    echo "$branch_name is release branch"
+}
+
+function main() {
+    check_branch_name
+    pip install pip-tools --upgrade
+    export -f freeze
+    find . -name "requirements.txt" | xargs -n 1 -I {} bash -c 'freeze "$@"' _ {}
+}
+
+main
