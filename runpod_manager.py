0fdd3dd514d2327a49ec4c7caa700f515a1d8be2
Sun, Xuehao <xuehao.sun@intel.com>
2026-02-26 15:04:32 +0800

    test
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- runpod_manager.py
+++ runpod_manager.py
@@ -130,7 +130,7 @@ def create_pod(args):
         # "imageName": args.image,
         "name": args.name,
         "volumeInGb": 0,
-        "templateId": "Azure-Ubuntu2404-AutoRound"
+        "templateId": "qtac2hjxru"
     }
 
     print(f"🚀 Creating pod: {args.name}...")
